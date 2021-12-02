81bd4e7652a677490224a70a3b5ef865a6ff7589
Andrea Angella <andrea.angella@red-gate.com>
2021-12-02 23:46:00 +0000

    Add Day1 and Day2

---
+++ 2021/AdventOfCode2021/Day1.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode2022
+{
+    [TestFixture]
+    public class Day1
+    {
+        private List<int> depths;
+
+        [SetUp]
+        public void SetUp()
+        {
+            depths = File.ReadAllLines("Day1.txt").Select(x => int.Parse(x)).ToList();
+        }
+
+        [Test]
+        public void Part1()
+        {
+            var result = 0;
+
+            for (int i = 1; i < depths.Count; i++)
+            {
+                if (depths[i] > depths[i - 1]) result++;
+            }
+
+            Assert.That(result, Is.EqualTo(1676));
+        }
+
+        [Test]
+        public void Part2()
+        {
+            var result = 0;
+
+            for (int i = 3; i < depths.Count; i++)
+            {
+                if (depths[i] + depths[i - 1] + depths[i - 2] > depths[i - 1] + depths[i - 2] + depths[i - 3])
+                {
+                    result++;
+                }
+            }
+
+            Assert.That(result, Is.EqualTo(1706));
+        }
+    }
+}
---
+++ 2021/AdventOfCode2021/Day2.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode2022
+{
+    [TestFixture]
+    public class Day2
+    {
+        List<(string Name, int Value)> commands;
+
+        [SetUp]
+        public void SetUp()
+        {
+            commands = File.ReadAllLines("Day2.txt").Select(x => (x.Split()[0], int.Parse(x.Split()[1]))).ToList();
+        }
+
+        [Test]
+        public void Part1()
+        {
+            var horizontal = 0;
+            var depth = 0;
+
+            foreach (var (Name, Value) in commands)
+            {
+                if (Name == "forward") horizontal += Value;
+                if (Name == "down")    depth += Value;
+                if (Name == "up")      depth -= Value;
+            }
+
+            Assert.That(horizontal * depth, Is.EqualTo(2272262));
+        }
+
+        [Test]
+        public void Part2()
+        {
+            var horizontal = 0;
+            var depth = 0;
+            var aim = 0;
+
+            foreach (var (Name, Value) in commands)
+            {
+                if (Name == "forward")
+                {
+                    horizontal += Value;
+                    depth += aim * Value;
+                }
+
+                if (Name == "down") aim += Value;
+                if (Name == "up")   aim -= Value;
+            }
+
+            Assert.That(horizontal * depth, Is.EqualTo(2134882034));
+        }
+    }
+}
