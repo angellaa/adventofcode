990b4e33ab050c86033adb9f7f77a8ab49e4a90c
Andrea Angella <andrea.angella@red-gate.com>
2021-12-04 05:52:56 +0000

    Solve Day 3 and 4

--- 2021/AdventOfCode2021/Day3.cs
+++ 2021/AdventOfCode2021/Day3.cs
@@ -5,24 +5,57 @@ namespace AdventOfCode2022
     [TestFixture]
     public class Day3
     {
-        List<string> input;
+        List<string> binaryNumbers;
+        int bits;
+        int n;
 
         [SetUp]
         public void SetUp()
         {
-            input = File.ReadAllLines("Day3.txt").ToList();
+            binaryNumbers = File.ReadAllLines("Day3.txt").ToList();
+            n = binaryNumbers.Count;
+            bits = binaryNumbers[0].Length;
         }
 
         [Test]
         public void Part1()
         {
-            Assert.That(-1, Is.EqualTo(0));
+            var gamma = new string(Enumerable.Range(0, bits)
+                                  .Select(i =>
+                                    binaryNumbers.Count(c => c[i] == '1') > (n / 2) ? '1' : '0'
+                                  ).ToArray());
+
+            var epsilon = new string(gamma.Select(x => x == '1' ? '0' : '1').ToArray());
+
+            Assert.That(Convert.ToInt64(gamma, 2) * Convert.ToInt64(epsilon, 2), Is.EqualTo(3912944));
         }
 
         [Test]
         public void Part2()
         {
-            Assert.That(-1, Is.EqualTo(0));
+            var numbers = binaryNumbers.ToList();
+
+            for (var i = 0; i < bits; i++)
+            {
+                var most = numbers.Count(c => c[i] == '1') >= numbers.Count(c => c[i] == '0') ? '1' : '0';
+                numbers.RemoveAll(x => x[i] != most);
+                if (numbers.Count == 1) break;
+            }
+
+            var oxygen = numbers.First();
+
+            numbers = binaryNumbers.ToList();
+
+            for (var i = 0; i < bits; i++)
+            {
+                var least = numbers.Count(c => c[i] == '1') < numbers.Count(c => c[i] == '0') ? '1' : '0';
+                numbers.RemoveAll(x => x[i] != least);
+                if (numbers.Count == 1) break;
+            }
+
+            var co2 = numbers.First();
+
+            Assert.That(Convert.ToInt64(oxygen, 2) * Convert.ToInt64(co2, 2), Is.EqualTo(4996233));
         }
     }
 }
--- 2021/AdventOfCode2021/Day4.cs
+++ 2021/AdventOfCode2021/Day4.cs
@@ -6,23 +6,110 @@ namespace AdventOfCode2022
     public class Day4
     {
         List<string> input;
+        List<int> numbers;
+        List<List<List<int>>> boards = new();
+        int n;
 
         [SetUp]
         public void SetUp()
         {
             input = File.ReadAllLines("Day4.txt").ToList();
+
+            numbers = input[0].Split(",").Select(x => int.Parse(x)).ToList();
+            n = (input.Count - 1) / 6;            
+
+            for (int i = 0; i < n; i++)
+            {
+                var board = new List<List<int>>();
+
+                for (int j = 0; j < 5; j++)
+                {
+                    board.Add(input[2 + 6 * i + j].Split().Where(x => x.Trim() != "").Select(x => int.Parse(x)).ToList());
+                }
+
+                boards.Add(board);
+            }
         }
 
         [Test]
         public void Part1()
         {
-            Assert.That(-1, Is.EqualTo(0));
+            Assert.That(Bingo(), Is.EqualTo(50008));
         }
+
 
         [Test]
         public void Part2()
         {
-            Assert.That(-1, Is.EqualTo(0));
+            Assert.That(Bingo2(), Is.EqualTo(17408));
+        }
+
+        private int Bingo()
+        {
+            foreach (var number in numbers)
+            {
+                foreach (var board in boards)
+                {
+                    for (var i = 0; i < 5; i++)
+                        for (var j = 0; j < 5; j++)
+                            if (board[i][j] == number)
+                            {
+                                board[i][j] = -1;
+                            }
+                }
+
+                foreach (var board in boards)
+                {
+                    for (var i = 0; i < 5; i++)
+                        if (board[i].Sum() == -5)
+                            return board.SelectMany(x => x).Where(x => x != -1).Sum() * number;
+
+                    for (var i = 0; i < 5; i++)
+                        if (board.Select(x => x[i]).Sum() == -5)
+                            return board.SelectMany(x => x).Where(x => x != -1).Sum() * number;
+                }
+            }
+
+            return 0;
+        }
+
+        private int Bingo2()
+        {
+            var lastBoard = new List<List<int>>();
+            var lastResult = 0;
+
+            foreach (var number in numbers)
+            {
+                foreach (var board in boards)
+                {
+                    for (var i = 0; i < 5; i++)
+                        for (var j = 0; j < 5; j++)
+                            if (board[i][j] == number)
+                            {
+                                board[i][j] = -1;
+                            }
+                }
+
+                var boardsToRemove = new List<List<List<int>>>();
+
+                foreach (var board in boards)
+                {
+                    for (var i = 0; i < 5; i++)
+                        if (board[i].Sum() == -5 || board.Select(x => x[i]).Sum() == -5)
+                        {
+                            lastBoard = board;
+                            lastResult = lastBoard.SelectMany(x => x).Where(x => x != -1).Sum() * number;
+                            boardsToRemove.Add(board);
+                        }
+                }
+
+                foreach (var board in boardsToRemove)
+                {
+                    boards.Remove(board);
+                }
+            }
+
+            return lastResult;
         }
     }
 }
