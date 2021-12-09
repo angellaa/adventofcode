082e61d15f3eed2ffc2048d43525cc48bfc608b7
Andrea Angella <andrea.angella@red-gate.com>
2021-12-09 08:49:31 +0000

    Add Day9 solution

--- 2021/AdventOfCode2021/Day9.cs
+++ 2021/AdventOfCode2021/Day9.cs
@@ -6,23 +6,99 @@ namespace AdventOfCode2022
     public class Day9
     {
         List<string> input;
+        bool[,] visited;
+        int n, m;
 
         [SetUp]
         public void SetUp()
         {
             input = File.ReadAllLines("Day9.txt").ToList();
+            n = input.Count;
+            m = input[0].Length;
         }
 
         [Test]
         public void Part1()
         {
-            Assert.That(-1, Is.EqualTo(0));
+            var result = 0;
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                    result += Risk(i, j);
+
+            Assert.That(result, Is.EqualTo(478));
+        }
+
+        private int Risk(int i, int j)
+        {
+            var value = Value(i, j);
+
+            if (value < Value(i-1, j) &&
+                value < Value(i+1, j) &&
+                value < Value(i, j-1) &&
+                value < Value(i, j+1))
+            {
+                return 1 + value;
+            }
+
+            return 0;
+        }
+
+        private int Value(int i, int j)
+        {
+            if (i < 0 || i >= n || j < 0 || j >= m) return int.MaxValue;
+
+            return input[i][j] - '0';
         }
 
         [Test]
         public void Part2()
         {
-            Assert.That(-1, Is.EqualTo(0));
+            long result = 1;
+            var basins = new List<long>();
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                    basins.Add(Size(i, j));
+
+            var largest = basins.OrderByDescending(x => x).Take(3);
+
+            foreach (var large in largest) result *= large;
+            
+            Assert.That(result, Is.EqualTo(1327014));
+        }
+
+        private int Size(int i, int j)
+        {
+            var value = Value(i, j);
+
+            if (value < Value(i - 1, j) &&
+                value < Value(i + 1, j) &&
+                value < Value(i, j - 1) &&
+                value < Value(i, j + 1))
+            {
+                visited = new bool[n, m];
+                return Basin(i, j);
+            }
+
+            return 1;
+        }
+
+        private bool Visited(int i, int j)
+        {
+            if (i < 0 || i >= n || j < 0 || j >= m) return true;
+
+            return visited[i,j];
+        }
+
+        private int Basin(int i, int j)
+        {
+            var value = Value(i, j);
+            if (value == 9 || Visited(i, j)) return 0;
+
+            visited[i,j] = true;
+
+            return 1 + Basin(i - 1, j) + Basin(i + 1, j) + Basin(i, j - 1) + Basin(i, j + 1);
         }
     }
 }
