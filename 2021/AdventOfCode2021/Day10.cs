08f8092d2cc46587935d84b5e0154b4981f79ed0
Andrea Angella <angella.andrea@gmail.com>
2021-12-10 05:30:21 +0000

    Add initial solution to Day 10

--- 2021/AdventOfCode2021/Day10.cs
+++ 2021/AdventOfCode2021/Day10.cs
@@ -1,28 +1,120 @@
-﻿using NUnit.Framework;
+﻿using System.Numerics;
+using NUnit.Framework;
 
 namespace AdventOfCode2022
 {
     [TestFixture]
     public class Day10
     {
-        List<string> input;
+        List<string> lines;
 
         [SetUp]
         public void SetUp()
         {
-            input = File.ReadAllLines("Day10.txt").ToList();
+            lines = File.ReadAllLines("Day10.txt").ToList();
         }
 
+        private static Dictionary<char, char> match = new()
+        { 
+            ['}'] = '{',
+            [')'] = '(',
+            [']'] = '[',
+            ['>'] = '<',
+        };
+
+        private static Dictionary<char, int> points = new()
+        {
+            ['}'] = 1197,
+            [')'] = 3,
+            [']'] = 57,
+            ['>'] = 25137,
+
+            ['{'] = 1197,
+            ['('] = 3,
+            ['['] = 57,
+            ['<'] = 25137,
+        };
+
+        private static Dictionary<char, int> points2 = new()
+        {
+            ['('] = 1,
+            ['['] = 2,
+            ['{'] = 3,
+            ['<'] = 4,
+        };
         [Test]
         public void Part1()
         {
-            Assert.That(-1, Is.EqualTo(0));
+            var result = 0;
+
+            foreach (var line in lines)
+            {
+                var stack = new Stack<char>();
+
+                foreach (var c in line)
+                {
+                    if (c is '(' or '{' or '[' or '<') stack.Push(c);
+                    else
+                    {
+                        var o = stack.Pop();
+                        if (match[c] != o)
+                        {
+                            result += points[c];
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Assert.That(result, Is.EqualTo(243939));
         }
 
         [Test]
         public void Part2()
         {
-            Assert.That(-1, Is.EqualTo(0));
+            var scores = new List<long>();
+
+            foreach (var line in lines)
+            {
+                var incomplete = true;
+                var stack = new Stack<char>();
+
+                foreach (var c in line)
+                {
+                    if (c is '(' or '{' or '[' or '<') stack.Push(c);
+                    else
+                    {
+                        var o = stack.Pop();
+                        if (match[c] != o)
+                        {
+                            incomplete = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (incomplete)
+                {
+                    long score = 0;
+                    while(stack.Count > 0)
+                    {
+                        var c = stack.Pop();
+                        score = (score * 5) + points2[c];
+                    }
+                    scores.Add(score);
+                }
+            }
+              
+            scores.Sort();
+
+            foreach (var score in scores)
+            {
+                Console.WriteLine(score);
+            }
+
+            var middle = scores[scores.Count / 2];
+
+            Assert.That(middle, Is.EqualTo(2421222841));
         }
     }
 }
