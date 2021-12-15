45a8a4b8d9c724b68d4ce3f39cfb8af344444f79
Andrea Angella <angella.andrea@gmail.com>
2021-12-15 06:24:37 +0000

    Add initial part2 solution for Day 15

--- 2021/AdventOfCode2021/Day15.cs
+++ 2021/AdventOfCode2021/Day15.cs
@@ -18,7 +18,6 @@ public class Day15
         rows = lines.Length;
         cols = lines[0].Length;
         input = lines.Select(x => x.Select(y => int.Parse(y.ToString())).ToList()).ToList();
-        input[0][0] = 0;
     }
 
     [Test]
@@ -29,6 +28,37 @@ public class Day15
         Assert.That(result, Is.EqualTo(403));
     }
     
+    [Test]
+    public void Part2()
+    {
+        for (var r = 0; r < rows; r++)
+        {
+            var original = input[r].ToList();
+
+            for (var i = 1; i < 5; i++)
+            {
+                input[r].AddRange(original.Select(x => 1 + (x - 1 + i) % 9));
+            }
+        }
+
+        for (var i = 1; i < 5; i++)
+        {
+            for (var r = 0; r < rows; r++)
+            {
+                var original = input[r].ToList();
+
+                input.Add(original.Select(x => 1 + (x - 1 + i) % 9).ToList());
+            }
+        }
+
+        rows = input.Count;
+        cols = input[0].Count;
+        
+        var result = AStar(0, rows * cols - 1);
+
+        Assert.That(result, Is.EqualTo(2840));
+    }
+
     int AStar(int start, int goal)
     {
         var openSet = new PriorityQueue<int, int>();
@@ -102,11 +132,4 @@ public class Day15
     private int Index(int r, int c) => r * rows + c;
     private int Cost(int i) => input[i / rows][i % cols];
     private int Cost(int r, int c) => Cost(Index(r, c));
-
-
-    [Test]
-    public void Part2()
-    {
-        Assert.That(-1, Is.EqualTo(0));
-    }
 }
