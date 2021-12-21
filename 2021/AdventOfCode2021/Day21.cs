53c2986ebbd6fdb2654382e2323f73307d9290bc
Andrea Angella <andrea.angella@red-gate.com>
2021-12-21 09:20:17 +0000

    Part1 of Day21

--- 2021/AdventOfCode2021/Day21.cs
+++ 2021/AdventOfCode2021/Day21.cs
@@ -6,18 +6,52 @@ namespace AdventOfCode2021;
 public class Day21
 {
     List<string> input;
+    private int pos1;
+    private int pos2;
+    private int score1;
+    private int score2;
+    private int die = 0;
+    private int rolls = 0;
 
     [SetUp]
     public void SetUp()
     {
         input = File.ReadAllLines("Day21.txt").ToList();
+        pos1 = int.Parse(input[0].Split(":")[1]) - 1;
+        pos2 = int.Parse(input[1].Split(":")[1]) - 1;
     }
 
     [Test]
     public void Part1()
     {
-        Assert.That(-1, Is.EqualTo(0));
+        while (score2 <= 1000)
+        {
+            pos1 = (pos1 + Roll()) % 10;
+            score1 += (pos1 + 1);
+
+            if (score1 >= 1000) break;
+
+            pos2 = (pos2 + Roll()) % 10;
+            score2 += (pos2 + 1);
+        }
+
+        Assert.That(rolls * Math.Min(score1, score2), Is.EqualTo(900099));
     }
+
+    public int Roll()
+    {
+        return NextDice() + NextDice() + NextDice();
+
+        int NextDice()
+        {
+            rolls++;
+            die++;
+            if (die > 100) die = 1;
+            return die;
+        }
+    }
+
+
 
     [Test]
     public void Part2()
