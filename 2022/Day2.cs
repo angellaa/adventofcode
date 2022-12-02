6b5b01d7e597cbadfefb0ef799f1a701b295922a
Andrea Angella <angella.andrea@gmail.com>
2022-12-02 16:10:39 +0000

    Day2 refactored

--- 2022/Day2.cs
+++ 2022/Day2.cs
@@ -5,123 +5,44 @@ namespace AdventOfCode2022;
 [TestFixture]
 public class Day2
 {
-    private List<(string First, string Second)> lines = new();
+    private List<(string FirstHand, string SecondHand)> rounds = new();
 
     [OneTimeSetUp]
     public void OneTimeSetUp()
     {
-        lines = File.ReadAllLines("Day2.txt").Select(x => (x.Split()[0], x.Split()[1])).ToList();
+        rounds = File.ReadAllLines("Day2.txt").Select(x => (x.Split()[0], x.Split()[1])).ToList();
     }
 
     [Test]
     public void Part1()
     {
-        var total = 0;
+        var scores = from round in rounds
+                     let opHand = Shape(round.FirstHand)
+                     let myHand = Shape(round.SecondHand)
+                     select HandScore(myHand) + RoundScore(myHand, opHand);
 
-        foreach (var line in lines)
-        {
-            var op = line.First switch
-            {
-                "A" => "R",
-                "B" => "P",
-                "C" => "S",
-                _ => ""
-            };
-
-            var you = line.Second switch
-            {
-                "X" => "R",
-                "Y" => "P",
-                "Z" => "S",
-                _ => ""
-            };
-
-            var score = scorePerShape(you) + (s: you, f: op) switch
-            {
-                ("R", "P") => 0,
-                ("R", "S") => 6,
-                ("P", "S") => 0,
-                ("P", "R") => 6,
-                ("S", "P") => 6,
-                ("S", "R") => 0,
-                _ => 3
-            };
-
-            int scorePerShape(string shape) => shape switch
-            {
-                "R" => 1,
-                "P" => 2,
-                "S" => 3,
-                _ => 0
-            };
-
-            total += score;
-        }
-
-        Assert.That(total, Is.EqualTo(8890));
+        Assert.That(scores.Sum(), Is.EqualTo(8890));
     }
 
     [Test]
     public void Part2()
     {
-        var total = 0;
+        var scores = from round in rounds
+                     let opHand = Shape(round.FirstHand)
+                     let myHand = round.SecondHand switch { "X" => LoosingShape(opHand), "Y" => opHand, "Z" => WinningShape(opHand) }
+                     select HandScore(myHand) + RoundScore(myHand, opHand);
 
-        foreach (var line in lines)
-        {
-            var op = line.First switch
-            {
-                "A" => "R",
-                "B" => "P",
-                "C" => "S",
-                _ => ""
-            };
-
-            var you = line.Second switch
-            {
-                "X" => loseShape(op),
-                "Y" => op,
-                "Z" => winShape(op),
-                _ => ""
-            };
-            
-            var score = scorePerShape(you) + (s: you, f: op) switch
-            {
-                ("R", "P") => 0,
-                ("R", "S") => 6,
-                ("P", "S") => 0,
-                ("P", "R") => 6,
-                ("S", "P") => 6,
-                ("S", "R") => 0,
-                _ => 3
-            };
-
-            int scorePerShape(string shape) => shape switch
-            {
-                "R" => 1,
-                "P" => 2,
-                "S" => 3,
-                _ => 0
-            };
-
-            string winShape(string shape) => shape switch
-            {
-                "R" => "P",
-                "P" => "S",
-                "S" => "R",
-                _ => ""
-            };
-
-            string loseShape(string shape) => shape switch
-            {
-                "P" => "R",
-                "S" => "P",
-                "R" => "S",
-                _ => ""
-            };
-
-            total += score;
-        }
-
-        Assert.That(total, Is.EqualTo(10238));
+        Assert.That(scores.Sum(), Is.EqualTo(10238));
     }
+
+    private static string Shape(string shape) => shape switch { "A" or "X" => "R", "B" or "Y" => "P", "C" or "Z" => "S" };
+    private static string WinningShape(string shape) => shape switch { "R" => "P", "P" => "S", "S" => "R" };
+    private static string LoosingShape(string shape) => shape switch { "P" => "R", "S" => "P", "R" => "S" };
+    private static int HandScore(string shape) => shape switch { "R" => 1, "P" => 2, "S" => 3 };
+    private static int RoundScore(string myHand, string opHand) => (myHand, opHand) switch
+    {
+        ("R", "S") or ("P", "R") or ("S", "P") => 6,
+        _ when (myHand == opHand) => 3,
+        _ => 0
+    };
 }
