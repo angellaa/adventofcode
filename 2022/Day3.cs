f4ae29fdae6ac5753860881fe7e152fe95b64464
Andrea Angella <angella.andrea@gmail.com>
2022-12-03 10:59:24 +0000

    Refactored Day 3

--- 2022/Day3.cs
+++ 2022/Day3.cs
@@ -5,28 +5,18 @@ namespace AdventOfCode2022;
 [TestFixture]
 public class Day3
 {
-    private List<string> lines = new();
+    private List<string> rucksacks = new();
 
     [OneTimeSetUp]
     public void OneTimeSetUp()
     {
-        lines = File.ReadAllLines("input.txt").ToList();
+        rucksacks = File.ReadAllLines("input.txt").ToList();
     }
 
     [Test]
     public void Part1()
     {
-        var total = 0;
-
-        foreach (var line in lines)
-        {
-            var a= line.Chunk(line.Length / 2).ToList()[0];
-            var b = line.Chunk(line.Length / 2).ToList()[1];
-            
-            var s = new string(a.Where(x => b.Contains(x)).Distinct().ToArray());
-
-            total += char.IsLower(s[0]) ? s[0] - 'a' + 1 : s[0] - 'A' + 27;
-        }
+        var total = rucksacks.Sum(r => Priority(r[..(r.Length / 2)].Intersect(r[(r.Length / 2)..]).First()));
 
         Assert.That(total, Is.EqualTo(8109));
     }
@@ -34,23 +24,12 @@ public class Day3
     [Test]
     public void Part2()
     {
-        var total = 0;
-
-        foreach (var chunk in lines.Chunk(3))
-        {
-            var s = chunk[0];
-
-            foreach (var line in chunk)
-            {
-                s = (new string(line.Where(x => s.Contains(x)).Distinct().ToArray()));
-            }
-
-            if (s != "")
-            {
-                total += char.IsLower(s[0]) ? s[0] - 'a' + 1 : s[0] - 'A' + 27;
-            }
-        }
+        var total = rucksacks.Chunk(3)
+                             .Sum(groups => Priority(groups.Aggregate((IEnumerable<char>) groups[0], (x, r) => x.Intersect(r))
+                             .First()));
 
         Assert.That(total, Is.EqualTo(2738));
     }
+
+    private static int Priority(char item) => char.IsLower(item) ? item - 'a' + 1 : item - 'A' + 27;
 }
