206431c8f053ea269787473e07945e79e009c663
Andrea Angella <angella.andrea@gmail.com>
2022-12-04 11:52:26 +0000

    Update Overlap to mean Overlap

--- 2022/Day4.cs
+++ 2022/Day4.cs
@@ -26,15 +26,18 @@ public class Day4
     [Test]
     public void Part2()
     {
-        var overlappedAssignments = assignments.Where(a => !a.First.Overlap(a.Second));
+        var overlappedAssignments = assignments.Where(a => a.First.Overlap(a.Second));
 
         Assert.That(overlappedAssignments.Count(), Is.EqualTo(909));
     }
 
     private record Assignment(int Start, int End)
     {
-        public bool Overlap(Assignment a) => End < a.Start || Start > a.End;
+        public bool Overlap(Assignment a) => !(End < a.Start || Start > a.End);
         public bool FullyContains(Assignment a) => Start <= a.Start && End >= a.End;
         public static Assignment Parse(string a) => new(int.Parse(a.Split("-")[0]), int.Parse(a.Split("-")[1]));
     }
+
+
+
 }
