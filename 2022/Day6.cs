9a84e578b96ad9a424bb0e8fa7a4c617169416bc
Andrea Angella <angella.andrea@gmail.com>
2022-12-06 07:42:44 +0000

    Day 6 solution

---
+++ 2022/Day6.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode2022;
+
+[TestFixture]
+public class Day6
+{
+    [TestCase(4, 1578)]
+    [TestCase(14, 2178)]
+    public void Test(int n, int expected)
+    {
+        var input = File.ReadAllText("Input.txt");
+
+        for (var i = n; i < input.Length; i++)
+        {
+            var sequence = input[(i - n)..i];
+
+            if (sequence.Distinct().SequenceEqual(sequence))
+            {
+                Assert.That(i, Is.EqualTo(expected));
+                return;
+            }
+        }
+
+        Assert.Fail();
+    }
+}
