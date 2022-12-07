11baca7f78c888a3745fb96625674c7de58761af
Andrea Angella <angella.andrea@gmail.com>
2022-12-07 09:21:42 +0000

    Minor refactor of Day 7

--- 2022/Day7.cs
+++ 2022/Day7.cs
@@ -1,68 +1,41 @@
 ﻿
 var lines = File.ReadAllLines("Input.txt");
 var currentDir = Dir.Root;
-var ls = false;
 
 foreach (var line in lines)
 {
     if (line.StartsWith("$ cd"))
     {
-        if (line == "$ cd /") currentDir = Dir.Root;
-        else if (line == "$ cd ..") currentDir = currentDir.Parent ?? Dir.Root;
+        if (line == "$ cd /")       currentDir = Dir.Root;
+        else if (line == "$ cd ..") currentDir = currentDir.Parent;
         else
         {
-            var dirName = line[2..].Split(" ")[1];
+            var name = line[2..].Split(" ")[1];
+            var existingDir = currentDir.Dirs.FirstOrDefault(x => x.Name.EndsWith(name + "/"));
 
-            var dir = currentDir.Dirs.FirstOrDefault(x => x.Name.EndsWith(dirName + "/"));
-
-            if (dir == null)
-            {
-                var newDir = new Dir(currentDir.Name + dirName + "/", currentDir);
-                currentDir.Dirs.Add(newDir);
-            }
-            else
-            {
-                currentDir = dir;
-            }
+            if (existingDir == null) currentDir.Dirs.Add(new Dir(currentDir.Name + name + "/", currentDir));
+            else                     currentDir = existingDir;
         }
-        ls = false;
     }
-    else if (line.StartsWith("$ ls"))
+    else if (line.StartsWith("$ ls")) {}
+    else
     {
-        ls = true;
-    }
-    else if (ls)
-    {
-        if (line.StartsWith("dir"))
-        {
-            currentDir.Dirs.Add(new Dir(currentDir.Name + line.Split(" ")[1] + "/", currentDir));
-        }
-        else
-        {
-            currentDir.Files.Add(new F(line.Split(" ")[1], int.Parse(line.Split(" ")[0])));
-        }
+        if (line.StartsWith("dir")) currentDir.Dirs.Add(new Dir(currentDir.Name + line.Split(" ")[1] + "/", currentDir));
+        else                        currentDir.Files.Add(new F(line.Split(" ")[1], int.Parse(line.Split(" ")[0])));
     }
 }
 
 Console.WriteLine(TotalPart1(Dir.Root));
 
 var freeSpace = 70000000 - Dir.Root.Size();
-var needSaved = 30000000 - freeSpace;
-var allDirs = GetDirs(Dir.Root);
+var spaceToFree = 30000000 - freeSpace;
 
-Console.WriteLine(allDirs.OrderBy(x => x.Size()).First(x => x.Size() >= needSaved).Size());
+Console.WriteLine(GetDirs(Dir.Root).OrderBy(x => x.Size()).First(x => x.Size() >= spaceToFree).Size());
 
 int TotalPart1(Dir d)
 {
     if (d == null) return 0;
-
-    var total = 0;
-    var size = d.Size();
-    if (size <= 100000) total += size;
-
-    total += d.Dirs.Sum(TotalPart1);
-
-    return total;
+    return (d.Size() <= 100000 ? d.Size() : 0) + d.Dirs.Sum(TotalPart1);
 }
 
 IEnumerable<Dir> GetDirs(Dir d)
@@ -75,8 +48,7 @@ IEnumerable<Dir> GetDirs(Dir d)
     yield return d;
 }
 
-
-record Dir(string Name, List<F> Files, List<Dir> Dirs, Dir Parent)
+internal record Dir(string Name, List<F> Files, List<Dir> Dirs, Dir Parent)
 {
     public static Dir Root = new("/");
     public Dir(string Name) : this(Name, new List<F>(), new List<Dir>(), null) { }
