3f393b011872e4807ecdbc6fae4aaa5912fd26d4
Andrea Angella <angella.andrea@gmail.com>
2022-12-07 09:02:59 +0000

    Made Day6 a bit more efficient

--- 2022/Day6.cs
+++ 2022/Day6.cs
@@ -24,4 +24,26 @@ public class Day6
 
         Assert.Fail();
     }
+
+    [TestCase(4, 1578)]
+    [TestCase(14, 2178)]
+    public void TestWithArray(int n, int expected)
+    {
+        var letters = new int[26];
+        var input = File.ReadAllText("Input.txt");
+
+        for (var i = 0; i < input.Length; i++)
+        {
+            if (i >= n) letters[input[i-n]-'a']--;
+            letters[input[i]-'a']++;
+
+            if (letters.Sum() == n && letters.All(x => x <= 1))
+            {
+                Assert.That(i + 1, Is.EqualTo(expected));
+                return;
+            }
+        }
+
+        Assert.Fail();
+    }
 }
