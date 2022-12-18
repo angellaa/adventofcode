d44899372a83eb883952d51dacecee13867beb9b
Andrea Angella <angella.andrea@gmail.com>
2022-12-18 21:03:18 +0000

    Day 18 part 1

---
+++ 2022/Day18.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode2022;
+
+[TestFixture]
+public class Day18
+{
+    private HashSet<(int, int, int)> cubes;
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        cubes = File.ReadAllLines("Input.txt")
+                    .Select(x => x.Split(",").ToArray())
+                    .Select(x => (int.Parse(x[0]), int.Parse(x[1]), int.Parse(x[2])))
+                    .ToHashSet();
+    }
+
+    [Test]
+    public void Part1()
+    {
+        var result = 0;
+
+        foreach (var (x, y, z) in cubes)
+        {
+            var adjacents = 0;
+
+            if (cubes.Contains((x - 1, y, z))) adjacents++;
+            if (cubes.Contains((x + 0, y, z))) adjacents++;
+            if (cubes.Contains((x + 1, y, z))) adjacents++;
+            if (cubes.Contains((x , y - 1, z))) adjacents++;
+            if (cubes.Contains((x , y + 0, z))) adjacents++;
+            if (cubes.Contains((x , y + 1, z))) adjacents++;
+            if (cubes.Contains((x , y, z - 1))) adjacents++;
+            if (cubes.Contains((x , y, z + 0))) adjacents++;
+            if (cubes.Contains((x , y, z + 1))) adjacents++;
+
+            result += 9 - adjacents;
+        }
+
+        Assert.That(result, Is.EqualTo(3650));
+    }
+
+    [Test]
+    public void Part2()
+    {
+    }
+}
