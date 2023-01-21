cdb51f6a224a2646ed7399575ada3c617ab0627d
Andrea Angella <angella.andrea@gmail.com>
2023-01-21 12:16:55 +0000

    Refactor Day16 as test

--- 2022/Day 16.cs
+++ 2022/Day 16.cs
@@ -1,112 +1,88 @@
-﻿//using System.Text.RegularExpressions;
+﻿using NUnit.Framework;
+using static System.Text.RegularExpressions.Regex;
 
-//var valves = new List<Valve>();
+[TestFixture]
+public class Day16
+{
+    private List<Valve> valves;
+    private Stack<Valve> openValves;
+    private HashSet<Valve> visited;
 
-//foreach (var line in File.ReadAllLines("Input.txt"))
-//{
-//    var m = Regex.Match(line, @"Valve (\w+) has flow rate=(\d+); tunnel[s]? lead[s]? to valve[s]? (.+)");
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        valves = new List<Valve>();
 
-//    var valve = m.Groups[1].Value;
-//    var flowRate = int.Parse(m.Groups[2].Value);
-//    var nextVales = m.Groups[3].Value.Split(", ").ToList();
+        foreach (var line in File.ReadAllLines("Input.txt"))
+        {
+            var m = Match(line, @"Valve (\w+) has flow rate=(\d+); tunnel[s]? lead[s]? to valve[s]? (.+)");
 
-//    valves.Add(new Valve(valve, flowRate, nextVales));
-//}
+            var valve = m.Groups[1].Value;
+            var flowRate = int.Parse(m.Groups[2].Value);
+            var nextVales = m.Groups[3].Value.Split(", ").ToList();
 
-//var openValves = new Stack<Valve>();
-//var visited = new HashSet<Valve>();
-//int best = 0;
+            valves.Add(new Valve(valve, flowRate, nextVales));
+        }
 
-//var maxOpenableValves = ExplorationSpace("AA").Count(x => valves.Find(y => y.Name == x).FlowRate > 0);
+        foreach (var valve in valves)
+        {
+            valve.NextValves = valve.NextValveNames.Select(x => valves.Find(v => v.Name == x)).OrderByDescending(x => x.FlowRate).ToList();
+        }
+    }
 
-//Console.WriteLine(MaxFlow(1, 0, valves.Find(x => x.Name == "AA"), 0));
-//Console.WriteLine("end");
+    [Test]
+    public void Part1()
+    {
+        openValves = new Stack<Valve>();
+        visited = new HashSet<Valve>();
 
-//int MaxFlow(int time, int totalFlow, Valve startValve, int movesWithoutOpens)
-//{
-//    if (openValves.Count >= maxOpenableValves || movesWithoutOpens > 3)
-//    {
-//        totalFlow += openValves.Sum(x => x.FlowRate) * (30 - time);
+        var maxFlow = MaxFlow(1, 0, valves.Find(x => x.Name == "AA"), 0);
 
-//        if (totalFlow > best)
-//        {
-//            Console.WriteLine("New best " + totalFlow);
-//            best = totalFlow;
-//        }
+        Assert.That(maxFlow, Is.EqualTo(1641));
+    }
 
-//        return totalFlow;
-//    }
+    [Test]
+    public void Part2() {}
 
-//    totalFlow += openValves.Sum(x => x.FlowRate);
+    private int MaxFlow(int time, int totalFlow, Valve startValve, int movesWithoutOpens)
+    {
+        if (movesWithoutOpens > 3)
+        {
+            return totalFlow + openValves.Sum(x => x.FlowRate) * (30 - time);
+        }
 
-//    if (time == 30)
-//    {
-//        if (totalFlow > best)
-//        {
-//            Console.WriteLine("New best " + totalFlow);
-//            best = totalFlow;
-//        }
-//        return totalFlow;
-//    }
+        totalFlow += openValves.Sum(x => x.FlowRate);
 
-//    visited.Add(startValve);
+        if (time == 30)
+        {
+            return totalFlow;
+        }
 
-//    var max = totalFlow;
+        visited.Add(startValve);
 
-//    if (!openValves.Contains(startValve) && startValve.FlowRate > 0)
-//    {
-//        openValves.Push(startValve);
-//        max = MaxFlow(time + 1, totalFlow, startValve, 0);
-//        openValves.Pop();
-//    }
+        var max = totalFlow;
 
-//    foreach (var valve in startValve.Next(valves))
-//    {
-//        max = Math.Max(max, MaxFlow(time + 1, totalFlow, valve, movesWithoutOpens + 1));
-//    }
+        if (!openValves.Contains(startValve) && startValve.FlowRate > 0)
+        {
+            openValves.Push(startValve);
 
-//    if (max > best)
-//    {
-//        Console.WriteLine("New best " + max);
-//        best = max;
-//    }
+            max = MaxFlow(time + 1, totalFlow, startValve, 0);
 
-//    visited.Remove(startValve);
+            openValves.Pop();
+        }
 
-//    return max;
-//}
+        foreach (var valve in startValve.NextValves)
+        {
+            max = Math.Max(max, MaxFlow(time + 1, totalFlow, valve, movesWithoutOpens + 1));
+        }
 
-//HashSet<string> ExplorationSpace(string valve)
-//{
-//    var toExplore = new Stack<string>();
-//    var result = new HashSet<string>();
+        visited.Remove(startValve);
 
-//    toExplore.Push(valve);
+        return max;
+    }
 
-//    while (toExplore.Count > 0)
-//    {
-//        var next = toExplore.Pop();
-
-//        if (result.Contains(next)) continue;
-
-//        result.Add(next);
-
-//        foreach (var nextValve in valves.Find(x => x.Name == next).NextValves)
-//        {
-//            toExplore.Push(nextValve);
-//        }
-//    }
-
-//    return result;
-//}
-
-
-//record Valve(string Name, int FlowRate, List<string> NextValves)
-//{
-//    private readonly List<Valve> nextValves = null;
-
-//    public List<Valve> Next(List<Valve> valves)
-//    {
-//        return nextValves ?? NextValves.Select(x => valves.Find(v => v.Name == x)).OrderByDescending(x => x.FlowRate).ToList();
-//    }
-//}
+    private record Valve(string Name, int FlowRate, List<string> NextValveNames)
+    {
+        public List<Valve> NextValves;
+    }
+}
